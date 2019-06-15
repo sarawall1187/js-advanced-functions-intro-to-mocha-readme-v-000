011bbb1b8f722adb216adfb6ee310572aaaaaff7
Sara Walker <sarawall1187@gmail.com>
2019-06-15 16:49:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theTruth(){
+  return true;
+}
+
+function  sayHey(){
+  return 'Hey!';
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(iceCream){
+  return `I love ${iceCream}`
+}
+
+function  shouting(shout){
+  return `${shout.toUpperCase()}`
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
